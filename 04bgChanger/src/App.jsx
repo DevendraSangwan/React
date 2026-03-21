c79c854e01843cfb8c72c720d35e5e907f747a99
DevendraSangwan <sangwandevendra658@gmail.com>
2026-03-21 13:34:24 +0530

    lecture 9 complete all

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
               Yellow
             </button>
 
+
             <button onClick={() => setColor("pink")}
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
               style={{ backgroundColor: "pink" }}>
@@ -50,6 +51,23 @@ function App() {
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
               style={{ backgroundColor: "green" }}>
               Green
+            </button>
+            <button onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "blue" }}>
+              Blue
+            </button><button onClick={() => setColor("orange")}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "orange" }}>
+              Orange
+            </button><button onClick={() => setColor("purple")}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "purple" }}>
+              Purple
+            </button><button onClick={() => setColor("brown")}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "brown" }}>
+              Brown
             </button>
 
           </div>
